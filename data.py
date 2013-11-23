b1eddb6aa79121bfb945c94c5c1a4410f3ad1abd
Eugine Blikh <bigbes@gmail.com>
2013-11-23 23:12:32 +0400

    Remove debug statements

--- data.py
+++ data.py
@@ -43,7 +43,7 @@ def init_filters():
     print cap_medians
     cap_medians = {k[0]: int(k[1]) for k in cap_medians}
 
-    feeds = list(set(sorted(open('db/feeds.txt').read().split('\n'))))[:3]
+    feeds = list(set(sorted(open('db/feeds.txt').read().split('\n'))))
     feeds.remove('')
     return (feeds, filters, cap_medians)
 
