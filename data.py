a909dc0fd3b3fc0e8f2b9ca04ed302ddb364b1f7
Eugine Blikh <bigbes@gmail.com>
2013-11-23 14:09:20 +0400

    Fixes

--- data.py
+++ data.py
@@ -9,6 +9,8 @@ import time
 import random
 import datetime
 
+from itertools import imap
+
 output_fname_db = 'db.lua'
 output_fname_wl = 'wl.lua'
 request = "box.insert('lists', '{0}', '{1}', {2});\n"
@@ -31,7 +33,7 @@ def init_filters():
 
     for k in filters_dir:
         name = k[1]
-        filters[name] = (open('db/'+k[0]).read().split('\n'))
+        filters[name] = open('db/'+k[0]).read().split('\n')
         filters[name].remove('')
         random.shuffle(filters[name])
         print "    '{0}' - {1}".format(name, len(filters[name]))
@@ -52,12 +54,14 @@ def generate_DB(feeds, filters, emitter):
     def select_list_filters(flist, num, used):
         tmp = len(used)
         for i in xrange(num):
-            used.add(repr(flist[gauss(len(flist))]))
+            used.add(flist[gauss(len(flist))])
         return (used, num - (len(used) - tmp))
 
+    print('-' * 30)
     print "Generating Database"
+    print('-' * 30)
+
     _time = None
-    print('0. _____')
     for num, _feed in enumerate(feeds):
         if not (_time is None):
             print "Cycle: {0} sec".format(str(time.clock() - _time))
@@ -67,7 +71,7 @@ def generate_DB(feeds, filters, emitter):
         sys.stdout.write(str(num + 1) + '. ')
         sys.stdout.flush()
         for _name, _filter in filters.iteritems():
-            a, b = (set(), int(random.uniform(0, len(_filter))))
+            a, b = (set(), int(random.uniform(0, len(_filter)))/10)
             precision, _try = (b/50, 0)
             while b > precision and _try < 10:
                 a, b = select_list_filters(_filter, b, a)
@@ -77,19 +81,19 @@ def generate_DB(feeds, filters, emitter):
             emitter(_feed, _name, a)
         sys.stdout.write('\n')
     print "Cycle: {0} sec".format(str(time.clock() - _time))
-    print ""
 
 def generate_WL(feeds, filters, number, emitter):
     def select_tuple_filters(_filters):
-        return [repr(v[gauss(len(v))]) for k, v in _filters.iteritems()]
+        return [v[gauss(len(v))] for k, v in _filters.iteritems()]
 
+    print('-' * 30)
     print "Generating Workload"
+    print('-' * 30)
     _time = time.clock()
     for i in xrange(number):
         emitter(feeds[gauss(len(feeds))], select_tuple_filters(filters))
     print "Done!"
     print "Cycle: {0} sec".format(str(time.clock() - _time))
-    print ""
 
 class Emitter_DB_TNT:
     def __init__(self, fname, sname):
@@ -97,7 +101,8 @@ class Emitter_DB_TNT:
         self.sname = sname
         self.request = "box.insert('{0}', '{1}', '{2}', {3});\n"
     def __call__(self, feed, ftype, values):
-        self.fwl.write(self.request.format(self.sname, feed, ftype, ', '.join(values)))
+        for v in values:
+            self.fwl.write(self.request.format(self.sname, feed, ftype, repr(v)))
     def __del__(self):
         self.fwl.close()
 
@@ -107,7 +112,7 @@ class Emitter_WL_TNT:
         self.sname = sname
         self.request = "box.insert('{0}', '{1}', {2});\n"
     def __call__(self, feed, values):
-        self.fwl.write(self.request.format(self.sname, feed, ', '.join(values)))
+        self.fwl.write(self.request.format(self.sname, feed, ", ".join(imap(repr, values))))
     def __del__(self):
         self.fwl.close()
 
@@ -117,10 +122,7 @@ if __name__ == '__main__':
     feeds, filters = init_filters()
     print ""
     print "Cycle: {0} sec".format(str(time.clock() - _time))
-    print ""
-    print('-' * 30)
     generate_DB(feeds, filters, Emitter_DB_TNT(output_fname_db, 'lists'))
-    print('-' * 30)
     generate_WL(feeds, filters, 100000, Emitter_WL_TNT(output_fname_wl, 'workload'))
     print('-' * 30)
     print "Overall time: {0} sec".format(str(time.clock() - _time))
